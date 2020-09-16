98ac4eb8e55b8c568d4e6d63c95cbae81430c2be
LzDarnGamer <48888484+LzDarnGamer@users.noreply.github.com>
2020-09-16 11:18:10 +0100

    03_implemtacao

---
+++ 03_Implementação/Scripts dos Itens/ItemDropHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class ItemDropHandler : MonoBehaviour, IDropHandler
+{
+    private DisplayInventory DisplayInv;
+
+
+    private void Start() {
+        DisplayInv = GameObject.FindGameObjectWithTag("MainCanvas").GetComponent<DisplayInventory>();
+    }
+    public void OnDrop(PointerEventData eventData) {
+        
+        if (eventData.pointerDrag != null 
+            && eventData.pointerDrag.GetComponent<Image>().sprite.name != "UISprite") {
+
+
+            
+            string PosOld = eventData.pointerDrag.name.Substring(7, eventData.pointerDrag.name.Length-7);
+            string Posnew = name.Substring(7, name.Length-7);
+
+            
+            DisplayInv.UpdatePostion(int.Parse(PosOld)-1, int.Parse(Posnew)-1);
+            
+
+    } 
+    }
+}
---
+++ 03_Implementação/Scripts dos Itens/LootTable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class LootTable 
+{
+    public string tablename;
+    public string[] pathName;
+    public int[] itemID;
+    public int[] minValue;
+    public int[] maxValue;
+}
